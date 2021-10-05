729bd556aa38b333207b0fd13b260fe78ba042b2
seifeslimene <s.slimene19@gmail.com>
2021-10-05 16:59:46 +0100

    Modify Message

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>This is new text to test change</p>
-        <p>Added By seif's branch!</p>
+        <p>Modified By seif's branch!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
